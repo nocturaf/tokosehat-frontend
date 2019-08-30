532664a6501923a21f579a2712a4a0b7b3650d15
nocturaf <nocturaf@gmail.com>
2019-08-30 16:22:55 +0700

    Home page

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+	$('.slick').slick({
+		  // infinite: false,
+		  // slidesToShow: 3,
+		  // slidesToScroll: 3,
+		  // dots: false,
+		  prevArrow: false,
+		  nextArrow: false,
+		  variableWidth: true,
+		  infinite: false,
+		  speed: 300,
+		  slidesToShow: 4,
+		  slidesToScroll: 4,	
+		 
+
+	});
+	
+
+});
