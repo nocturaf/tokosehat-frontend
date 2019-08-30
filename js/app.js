96dd6a2f8ddd407a43e0b5260fc63926c1e3763f
nocturaf <nocturaf@gmail.com>
2019-08-30 19:17:52 +0700

    progress home page

--- js/app.js
+++ js/app.js
@@ -5,7 +5,29 @@ $(document).ready(function () {
 	 */
 
 	// Product slider using slick
+	$('.recommend').slick({
+		  // infinite: false,
+		  // slidesToShow: 3,
+		  // slidesToScroll: 3,
+		  // dots: false,
+		  prevArrow: false,
+		  nextArrow: false,
+		  variableWidth: true,
+		  infinite: false,
+		  speed: 300,
+		  slidesToShow: 4,
+		  slidesToScroll: 4,	
+	});
 
+	$('.vegetarian').slick({
+		  prevArrow: false,
+		  nextArrow: false,
+		  variableWidth: true,
+		  infinite: false,
+		  speed: 300,
+		  slidesToShow: 4,
+		  slidesToScroll: 4,	
+	});
 
 
 	/**
@@ -24,47 +46,29 @@ $(document).ready(function () {
 	   url: HOST+'/recipes',
 	   success: function(data) {        
 	   	let listFood = data.results;
+	   	console.log(listFood);
 	   	let limit = 5;
-	   	for (var i = 0; i < 2; i++) {
-   			let foodList = 
-   			"<div class='slick-slide slick-current slick-active' data-slick-index='"+i+"' aria-hidden='false' id='foodCard'>"+
-   				"<div>"+
-	   				"<div class='col-md-3' id='foodCard'>"+
-						"<div class='card' style='width: 18rem;'>"+
-						  "<img src='"+listFood[i].image+"' class='card-img-top'>"+
-						  "<div class='card-body'>"+
-						    "<h5 class='card-title'>"+listFood[i].title+"</h5>"+
-						    "<a href='#' class='badge badge-primary'>Snack</a>"+
-						    "<a href='#' class='badge badge-success'>Roti</a>"+
-						    "<br><br>"+
-						    "<p style='font-family: "+'Lato, sans-serif;'+"class='card-text'>Manfaat : Bebas Gula</p>"+
-						    "<a href='#' class='btn background-green'>Selengkapnya</a>"+
-						  "</div>"+
-						"</div>"+
+	   	for (var i = 0; i < limit; i++) {
+	
+   			let foodList = "<div id='foodList' class='row'>"+
+   				"<div class='col-md-3 ml-3'>"+
+					"<div class='card' style='width: 18rem; height: 400px;'>"+
+					  "<img src='"+listFood[i].image+"' class='card-img-top' style='height:180px;'>"+
+					  "<div class='card-body'>"+
+					    "<h5 class='card-title'>"+listFood[i].title+"</h5>"+
+					    "<a href='#' class='badge badge-primary'>Snack</a>"+
+					    "<a href='#' class='badge badge-success'>Roti</a>"+
+					    "<br><br>"+
+					    "<p style='font-family: "+'Lato, sans-serif;'+"class='card-text'>Manfaat : "+listFood[i].benefit+"</p>"+
+					    "<a href='#' class='btn background-green'>Selengkapnya</a>"+
+					  "</div>"+
 					"</div>"+
 				"</div>"+
 			"</div>";
-
-			$('#foodList .slick-list .slick-track').append(
-				foodList
-			);
+			$('.recommend').slick('slickAdd', foodList);
 	   	}
 	     
 	   }
-	});
-
-		$('.slick').slick({
-		  // infinite: false,
-		  // slidesToShow: 3,
-		  // slidesToScroll: 3,
-		  // dots: false,
-		  prevArrow: false,
-		  nextArrow: false,
-		  variableWidth: true,
-		  infinite: false,
-		  speed: 300,
-		  slidesToShow: 4,
-		  slidesToScroll: 4,	
 	});
 	
 
