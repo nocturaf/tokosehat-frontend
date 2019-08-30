4f3831fbc005c701477b4456f19ad5d5720a575d
nocturaf <nocturaf@gmail.com>
2019-08-30 18:30:56 +0700

    integrate slick but error

--- js/app.js
+++ js/app.js
@@ -1,6 +1,59 @@
 $(document).ready(function () {
 
-	$('.slick').slick({
+	/**
+	 * APPS AND DOMS
+	 */
+
+	// Product slider using slick
+
+
+
+	/**
+	 * API CALLS
+	 */
+	
+	// credentials
+	const HOST = 'http://35.240.133.181/api';
+
+
+
+	// get recipes
+	$.ajax({ 
+	   type: 'GET',
+	   dataType: 'json',
+	   url: HOST+'/recipes',
+	   success: function(data) {        
+	   	let listFood = data.results;
+	   	let limit = 5;
+	   	for (var i = 0; i < 2; i++) {
+   			let foodList = 
+   			"<div class='slick-slide slick-current slick-active' data-slick-index='"+i+"' aria-hidden='false' id='foodCard'>"+
+   				"<div>"+
+	   				"<div class='col-md-3' id='foodCard'>"+
+						"<div class='card' style='width: 18rem;'>"+
+						  "<img src='"+listFood[i].image+"' class='card-img-top'>"+
+						  "<div class='card-body'>"+
+						    "<h5 class='card-title'>"+listFood[i].title+"</h5>"+
+						    "<a href='#' class='badge badge-primary'>Snack</a>"+
+						    "<a href='#' class='badge badge-success'>Roti</a>"+
+						    "<br><br>"+
+						    "<p style='font-family: "+'Lato, sans-serif;'+"class='card-text'>Manfaat : Bebas Gula</p>"+
+						    "<a href='#' class='btn background-green'>Selengkapnya</a>"+
+						  "</div>"+
+						"</div>"+
+					"</div>"+
+				"</div>"+
+			"</div>";
+
+			$('#foodList .slick-list .slick-track').append(
+				foodList
+			);
+	   	}
+	     
+	   }
+	});
+
+		$('.slick').slick({
 		  // infinite: false,
 		  // slidesToShow: 3,
 		  // slidesToScroll: 3,
@@ -12,8 +65,6 @@ $(document).ready(function () {
 		  speed: 300,
 		  slidesToShow: 4,
 		  slidesToScroll: 4,	
-		 
-
 	});
 	
 
